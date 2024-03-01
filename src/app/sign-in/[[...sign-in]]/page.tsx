47f843c3264ce97757a9bbacb883bdbfd7a8dfd6
jma49 <majincheng990128@gmail.com>
2024-02-29 22:26:25 -0600

    feat: modify app router

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,6 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-    <div className="flex justify-center py-24">
         <SignIn />
-    </div>
     );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,6 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-    <div className="flex justify-center py-24">
-        <SignUp />
-    </div>
+        <SignUp />   
     );
 }
